f35bb8bf645cbddb2bff37726be7df5892648882
czetoj <czeto.jozsef.0229@gmail.com>
2021-01-25 15:56:07 +0100

    tooltip

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,13 @@
-$('[data-toggle="tooltip"]').tooltip();
+
 const navbar = document.querySelector('.navbar');
 const navlinks = document.querySelectorAll('.cus--navlink');
+const facebook = document.querySelector('.facebook-blue');
+const twitter = document.querySelector('.twitter-blue');
+const google = document.querySelector('.google-red');
+const tooltipfacebook = document.querySelector('.tooltip-facebook');
+const tooltiptwitter = document.querySelector('.tooltip-twitter');
+const tooltipgoogle = document.querySelector('.tooltip-google');
+
 
 document.addEventListener('scroll', () => {
     navbar.classList.remove('bg-transparent');
@@ -11,6 +18,27 @@ document.addEventListener('scroll', () => {
 
     navlinks.forEach(item => item.style.color = 'grey');
 })
+
+
+facebook.addEventListener('mouseenter', () => {
+    tooltipfacebook.style.display = 'block';
+})
+twitter.addEventListener('mouseenter', () => {
+    tooltiptwitter.style.display = 'block';
+})
+google.addEventListener('mouseenter', () => {
+    tooltipgoogle.style.display = 'block';
+})
+facebook.addEventListener('mouseleave', () => {
+    tooltipfacebook.style.display = 'none';
+})
+twitter.addEventListener('mouseleave', () => {
+    tooltiptwitter.style.display = 'none';
+})
+google.addEventListener('mouseleave', () => {
+    tooltipgoogle.style.display = 'none';
+})
+
 
 var scroll = new SmoothScroll('a[href*="#"]', {
     speed: 300
